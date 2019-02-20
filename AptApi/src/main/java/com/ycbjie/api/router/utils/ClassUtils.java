c1134314e90a9d53afebec9294f12f2d405bb8e9
yangchong211 <yangchong211@163.com>
2019-02-20 14:28:44 +0800

    apt方案实践与探索

--- AptApi/src/main/java/com/ycbjie/api/router/utils/ClassUtils.java
+++ AptApi/src/main/java/com/ycbjie/api/router/utils/ClassUtils.java
@@ -59,35 +59,37 @@ public class ClassUtils {
         final CountDownLatch countDownLatch = new CountDownLatch(paths.size());
         ThreadPoolExecutor threadPoolExecutor = DefaultPoolExecutor.newDefaultPoolExecutor(paths.size());
         for (final String path : paths) {
-            threadPoolExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    DexFile dexFile = null;
-                    try {
-                        //加载 apk中的dex 并遍历 获得所有包名为 {packageName} 的类
-                        dexFile = new DexFile(path);
-                        Enumeration<String> dexEntries = dexFile.entries();
-                        while (dexEntries.hasMoreElements()) {
-                            String className = dexEntries.nextElement();
-                            if (!TextUtils.isEmpty(className) && className.startsWith(packageName)) {
-                                classNames.add(className);
+            if (threadPoolExecutor != null) {
+                threadPoolExecutor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        DexFile dexFile = null;
+                        try {
+                            //加载 apk中的dex 并遍历 获得所有包名为 {packageName} 的类
+                            dexFile = new DexFile(path);
+                            Enumeration<String> dexEntries = dexFile.entries();
+                            while (dexEntries.hasMoreElements()) {
+                                String className = dexEntries.nextElement();
+                                if (!TextUtils.isEmpty(className) && className.startsWith(packageName)) {
+                                    classNames.add(className);
+                                }
                             }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        if (null != dexFile) {
-                            try {
-                                dexFile.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            if (null != dexFile) {
+                                try {
+                                    dexFile.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
                             }
+                            //释放一个
+                            countDownLatch.countDown();
                         }
-                        //释放一个
-                        countDownLatch.countDown();
                     }
-                }
-            });
+                });
+            }
         }
         //等待执行完成
         countDownLatch.await();
