6c64bb381f20046a29f4be83cbb0a22185126d12
yangchong211 <yangchong211@163.com>
2019-02-20 17:59:57 +0800

    apt方案实践与探索

--- app/src/main/java/com/ycbjie/ycapt/FirstActivity.java
+++ app/src/main/java/com/ycbjie/ycapt/FirstActivity.java
@@ -4,14 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.ycbjie.annotation.contentView.ContentView;
 import com.ycbjie.api.click.OnceInit;
 import com.ycbjie.annotation.click.OnceClick;
+import com.ycbjie.api.contentView.ContentActivity;
 
 
-@ContentView(R.layout.activity_six)
-public class FirstActivity extends AppCompatActivity {
+@ContentView(R.layout.activity_first)
+public class FirstActivity extends ContentActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,10 +21,11 @@ public class FirstActivity extends AppCompatActivity {
         OnceInit.once(this,2000);
     }
 
-
+    int a = 1;
     @OnceClick(R.id.tv_1)
     public void Click1(){
         Log.d("tag--------------------","tv_1");
+        Toast.makeText(this, "点击事件，间隔两秒"+ a++, Toast.LENGTH_SHORT).show();
     }
 
 
