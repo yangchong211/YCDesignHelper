f8346915fec8c59ac7f5bec773ddb94d33412ef2
yangchong211 <yangchong211@163.com>
2019-02-21 11:39:32 +0800

    apt方案实践与探索

--- AptCompiler/src/main/java/com/ycbjie/compiler/router/RouterConstants.java
+++ AptCompiler/src/main/java/com/ycbjie/compiler/router/RouterConstants.java
@@ -12,53 +12,52 @@ package com.ycbjie.compiler.router;
  */
 public class RouterConstants {
 
-    public static final String SEPARATOR = "_";
-    public static final String PROJECT = "ARouter";
-    public static final String NAME_OF_GROUP = PROJECT + SEPARATOR + "Group" + SEPARATOR;
-    public static final String NAME_OF_ROOT = PROJECT + SEPARATOR + "Root" + SEPARATOR;
+    private static final String SEPARATOR = "_";
+    static final String PROJECT = "ARouter";
+    static final String NAME_OF_GROUP = PROJECT + SEPARATOR + "Group" + SEPARATOR;
+    static final String NAME_OF_ROOT = PROJECT + SEPARATOR + "Root" + SEPARATOR;
 
-    public static final String PATH = "com.ycbjie.api.router";
-    public static final String I_SERVICE = "com.ycbjie.api.router.inter.IService";
-    public static final String I_ROUTE_GROUP = "com.ycbjie.api.router.inter.IRouteGroup";
-    public static final String I_ROUTE_ROOT = "com.ycbjie.api.router.inter.IRouteRoot";
-    public static final String I_EXTRA = "com.ycbjie.api.router.inter.IExtra";
-    public static final String ANN_TYPE_EXTRA = "com.ycbjie.annotation.router.Extra";
-    public static final String PACKAGE_OF_GENERATE_FILE = "com.ycbjie.api.router.routes";
+    static final String PATH = "com.ycbjie.api.router";
+    static final String I_SERVICE = "com.ycbjie.api.router.inter.IService";
+    static final String I_ROUTE_GROUP = "com.ycbjie.api.router.inter.IRouteGroup";
+    static final String I_ROUTE_ROOT = "com.ycbjie.api.router.inter.IRouteRoot";
+    static final String I_EXTRA = "com.ycbjie.api.router.inter.IExtra";
+    static final String ANN_TYPE_EXTRA = "com.ycbjie.annotation.router.Extra";
+    static final String PACKAGE_OF_GENERATE_FILE = "com.ycbjie.api.router.routes";
 
-    public static final String ARGUMENTS_NAME = "moduleName";
-    public static final String ACTIVITY = "android.app.Activity";
+    static final String ARGUMENTS_NAME = "moduleName";
+    static final String ACTIVITY = "android.app.Activity";
 
-    public static final String METHOD_LOAD_INTO = "loadInto";
-    public static final String METHOD_LOAD_EXTRA = "loadExtra";
+    static final String METHOD_LOAD_INTO = "loadInto";
+    static final String METHOD_LOAD_EXTRA = "loadExtra";
 
-
-    public static final String PARCELABLE = "android.os.Parcelable";
+    static final String PARCELABLE = "android.os.Parcelable";
 
     private static final String LANG = "java.lang";
     public static final String BYTE = LANG + ".Byte";
     public static final String SHORT = LANG + ".Short";
-    public static final String INTEGER = LANG + ".Integer";
+    static final String INTEGER = LANG + ".Integer";
     public static final String LONG = LANG + ".Long";
     public static final String FLOAT = LANG + ".Float";
     public static final String DOUBEL = LANG + ".Double";
     public static final String BOOLEAN = LANG + ".Boolean";
-    public static final String STRING = LANG + ".String";
+    static final String STRING = LANG + ".String";
     public static final String ARRAY = "ARRAY";
 
-    public static final String ARRAYLIST = "java.util.ArrayList";
-    public static final String LIST = "java.util.List";
+    static final String ARRAYLIST = "java.util.ArrayList";
+    static final String LIST = "java.util.List";
 
-    public static final String BYTEARRAY = "byte[]";
-    public static final String SHORTARRAY = "short[]";
-    public static final String BOOLEANARRAY = "boolean[]";
-    public static final String CHARARRAY = "char[]";
-    public static final String DOUBLEARRAY = "double[]";
-    public static final String FLOATARRAY = "float[]";
-    public static final String INTARRAY = "int[]";
-    public static final String LONGARRAY = "long[]";
-    public static final String STRINGARRAY = "java.lang.String[]";
+    static final String BYTEARRAY = "byte[]";
+    static final String SHORTARRAY = "short[]";
+    static final String BOOLEANARRAY = "boolean[]";
+    static final String CHARARRAY = "char[]";
+    static final String DOUBLEARRAY = "double[]";
+    static final String FLOATARRAY = "float[]";
+    static final String INTARRAY = "int[]";
+    static final String LONGARRAY = "long[]";
+    static final String STRINGARRAY = "java.lang.String[]";
 
 
-    public static final String NAME_OF_EXTRA = SEPARATOR + "Extra";
+    static final String NAME_OF_EXTRA = SEPARATOR + "Extra";
 
 }
